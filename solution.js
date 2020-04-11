ce6141b630b246df429cd50f4552be7f4e9a43a4
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-04-10 23:40:08 -0400

    Add class demo, work on Part 2 and Bonus, further debugging needed

--- solution.js
+++ solution.js
@@ -5,7 +5,7 @@ document.querySelector("link").href = "./styles.css";
 
 /* 1.2) Set the class of the image to "list-img" */
 
-document.querySelector("img").className = "list-img";
+document.querySelector(".header img").className = "list-img";
 
 /* 1.3) Set the classes of the inputs (buttons) to "btn" by using a loop */
 
@@ -19,7 +19,8 @@ for (i = 0; i < buttons.length; i++) {
 
 /* 2.1) grab the <ul> that we are going to append <li> nodes to and store it in a variable */
 
-
+let list = document.querySelectorAll("#grocery-list ul");
+console.log(list);
 
 /* 2.2) When the user clicks the ADD button, ask them what item they need to add and then:
         1. Create a new li Element
@@ -27,19 +28,49 @@ for (i = 0; i < buttons.length; i++) {
         3. Append the text node to the li Element
         4. Add the li Element to the unordered list by appending it to the variable created in step 2.1 */
 
-
-
-
+function addItem() {
+        let item = prompt(`Please enter the item that you'd like to add to the list:`);
+        console.log(item);
+        let element = document.createElement("li");
+        console.log(element);
+        let textNode = document.createTextNode(item);
+        console.log(textNode);
+        element.appendChild(textNode);
+        list.appendChild(element);
+// Console error message for line 39:
+// Uncaught TypeError: list.appendChild is not a function at addItem
+        console.log(list);
+}
 
 /* 2.3) When the user clicks the TOTAL button:
         1. Display the total area by setting the total-area class to just "total-area"
         2. Total the number of groceries by getting the length of the li NodeList
         3. Add the count to the page by setting the textContent of #item-count */
 
-
-
+function getTotal() {
+        let totalArea = document.querySelector("div .total-area");
+        console.log(totalArea);
+        totalArea.className = "total-area";
+        let groceries = document.querySelectorAll("#grocery-list li");
+        let numberGroceries = groceries.length;
+        document.getElementById("item-count").textContent = numberGroceries;
+}
 
 ////////////// BONUS /////////////////////
 /*  When the user clicks the REMOVE button, ask them what item they want to remove and then:
     1. Remove the item from the list with removeChild by comparing the user's response to the textContent of each <li> */
 
+function removeItem() {
+        let itemToBeRemoved = prompt(`Please enter the item that you'd like to remove from thte list:`);
+        let list = document.querySelectorAll("#grocery-list ul");
+        let groceries = document.querySelectorAll("#grocery-list li");
+        for (i = 0; i < groceries.length; i++) {
+             if (groceries[i].textContent === itemToBeRemoved) {
+                list.removeChild(groceries[i]);
+// Console error message for line 67:
+// Uncaught TypeError: list.removeChild is not a function at removeItem
+             } else {
+                break;
+             }
+        }
+}
